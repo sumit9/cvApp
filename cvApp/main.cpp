b236a22157f5b45fd02bfb059c9ba78b1292cc5a
nhonaitran <ai.tran@nascentlab.com>
2013-07-02 14:18:02 -0500

    Feature extraction added

--- cvApp/main.cpp
+++ cvApp/main.cpp
@@ -38,7 +38,7 @@ void featureDetectionSimpleMatching(string img, string img2) {
 	}
 
 	//detecting keypoints
-	SurfFeatureDetector detector(3000);
+	SurfFeatureDetector detector(1500);
 	vector<KeyPoint> keypt1, keypt2;
 	detector.detect(image1, keypt1);
 	detector.detect(image2, keypt2);
@@ -62,7 +62,72 @@ void featureDetectionSimpleMatching(string img, string img2) {
 	imshow(window, img_matches);
 }
 
-void featureDetectionORBAlgo(string img, string img2) {
+void featureDetectionORB(string img, string img2) {
+	Mat image1 = imread(img, CV_LOAD_IMAGE_GRAYSCALE);
+	Mat image2 = imread(img2, CV_LOAD_IMAGE_GRAYSCALE);
+	if (image1.empty() || image2.empty()) {
+		cout << "Could not open or find one of the image files." << endl;
+		return;
+	}
+
+	//detecting keypoints
+	OrbFeatureDetector detector(40);
+	vector<KeyPoint> keypt1, keypt2;
+	detector.detect(image1, keypt1);
+	detector.detect(image2, keypt2);
+
+	//compute descriptors
+	OrbDescriptorExtractor extractor;
+	Mat descriptor1, descriptor2;
+	extractor.compute(image1, keypt1, descriptor1);
+	extractor.compute(image2, keypt2, descriptor2);
+
+	//matching descriptors
+	BFMatcher matcher(NORM_L2);
+	vector<DMatch> matches;
+	matcher.match(descriptor1, descriptor2, matches);
+
+	//drawing the results
+	string window = "Matching Results";
+	namedWindow(window, 1);
+	Mat img_matches;
+	drawMatches(image1, keypt1, image2, keypt2, matches, img_matches);
+	imshow(window, img_matches);
+	imwrite("featureDetectionORB.jpg", img_matches);
+}
+
+void featureDetectionBRIEF(string img, string img2) {
+	Mat image1 = imread(img, CV_LOAD_IMAGE_GRAYSCALE);
+	Mat image2 = imread(img2, CV_LOAD_IMAGE_GRAYSCALE);
+	if (image1.empty() || image2.empty()) {
+		cout << "Could not open or find one of the image files." << endl;
+		return;
+	}
+
+	//detecting keypoints
+	OrbFeatureDetector detector(40);
+	vector<KeyPoint> keypt1, keypt2;
+	detector.detect(image1, keypt1);
+	detector.detect(image2, keypt2);
+
+	//compute descriptors
+	BriefDescriptorExtractor extractor;
+	Mat descriptor1, descriptor2;
+	extractor.compute(image1, keypt1, descriptor1);
+	extractor.compute(image2, keypt2, descriptor2);
+
+	//matching descriptors
+	BFMatcher matcher(NORM_L2);
+	vector<DMatch> matches;
+	matcher.match(descriptor1, descriptor2, matches);
+
+	//drawing the results
+	string window = "Matching Results";
+	namedWindow(window, 1);
+	Mat img_matches;
+	drawMatches(image1, keypt1, image2, keypt2, matches, img_matches);
+	imshow(window, img_matches);
+	imwrite("featureDetectionBRIEF.jpg", img_matches);
 }
 
 int main(int argc, char* argv[]) 
@@ -76,6 +141,14 @@ int main(int argc, char* argv[])
 		case 1:
 			img2 = argv[3];
 			featureDetectionSimpleMatching(img, img2);
+			break;
+		case 2:
+			img2 = argv[3];
+			featureDetectionORB(img, img2);
+			break;
+		case 3:
+			img2 = argv[3];
+			featureDetectionBRIEF(img, img2);
 			break;
 		default:
 			loadImage(img);
