64009955034194a1eb16875d898c22687b498383
nhonaitran <ai.tran@nascentlab.com>
2013-07-03 08:29:27 -0500

    find contours

--- cvApp/main.cpp
+++ cvApp/main.cpp
@@ -130,11 +130,69 @@ void featureDetectionBRIEF(string img, string img2) {
 	imwrite("featureDetectionBRIEF.jpg", img_matches);
 }
 
+void detectContours(string imageName) {
+	//load the specified image
+	Mat src = imread(imageName, CV_LOAD_IMAGE_GRAYSCALE);
+	if (src.empty()) {
+		cout << "Could not open or find the image file." << endl;
+		return;
+	}
+
+    namedWindow( "Source", 1 );
+    imshow( "Source", src );
+
+	//threshold grayscale image to black-white image 
+	src = src > 128;
+    namedWindow( "Source BW", 1 );
+    imshow( "Source BW", src );
+
+	Mat dst = Mat::zeros(src.rows, src.cols, CV_8UC3);
+	vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+
+    findContours( src, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
+
+    // iterate through all the top-level contours, draw each connected component with its own random color
+    int idx = 0;
+    for( ; idx >= 0; idx = hierarchy[idx][0] )
+    {
+        Scalar color( rand()&255, rand()&255, rand()&255 );
+        drawContours( dst, contours, idx, color, CV_FILLED, 8, hierarchy );
+    }
+
+    namedWindow( "Components", 1 );
+    imshow( "Components", dst );
+}
+
+void detectForestUsingBRIEF(string imageName) {
+	Mat image = imread(imageName, CV_LOAD_IMAGE_GRAYSCALE);
+	if (image.empty()) {
+		cout << "Could not open or find the image file." << endl;
+		return;
+	}
+
+	//detecting keypoints
+	OrbFeatureDetector detector(40);
+	vector<KeyPoint> keypt;
+	detector.detect(image, keypt);
+
+	//compute descriptors
+	BriefDescriptorExtractor extractor;
+	Mat descriptor;
+	extractor.compute(image, keypt, descriptor);
+
+	//drawing the results
+	string window = "Results";
+	namedWindow(window, CV_WINDOW_AUTOSIZE);
+	imshow(window, descriptor);
+	//imwrite("featureDetectionBRIEF.jpg", img_matches);
+}
+
 int main(int argc, char* argv[]) 
 {
-	string sample = argv[1];
+	string sample = (argc > 1) ? argv[1] : "4";
 	int option = atoi(sample.c_str());
-	string img = argv[2];
+	string img = (argc > 1) ? argv[2] : "C:\\Projects\\cvApp\\x64\\Debug\\deforestation-5.jpg";
 	string img2;
 	switch (option)
 	{
@@ -149,7 +207,11 @@ int main(int argc, char* argv[])
 		case 3:
 			img2 = argv[3];
 			featureDetectionBRIEF(img, img2);
+		case 4:
+			detectContours(img);
 			break;
+		case 5:
+			detectForestUsingBRIEF(img);
 		default:
 			loadImage(img);
 			break;
